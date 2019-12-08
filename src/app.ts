2e115c71572b1d66746e5e63d796469c75da20fa
Lev Simionenko <levsim2016@gmail.com>
2019-12-09 00:01:16 +0300

    use database to connect to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
+
 import { apiRouter } from './api/apiRouter';
+import { Database } from './datastore/Database';
 
 export const server: express.Application = express();
 
-server.use('/api', apiRouter)
+server.use(express.json());
+server.use('/api', apiRouter);
 
-server.listen(3200, function () {
-    console.log('Server listening on port 3200!');
+const mongodbClient = new MongoClient('mongodb://localhost:27017/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongodbClient.connect(function (error, client) {
+    if (error) {
+        console.log(error);
+    }
+
+    Database.init(client.db('tsk-manager'));
+    Database.createCollectionIndex('tasks', 'id', {
+        unique: true,
+    })
+
+    server.listen(3200, function () {
+        console.log('Server listening on port 3200!');
+    });
 });
